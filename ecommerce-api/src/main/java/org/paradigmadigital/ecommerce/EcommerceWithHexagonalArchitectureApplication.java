a8b97e8accc34d8b8e255f1832a8ee574270c514
Ismail Ahmedov <ismail2ov@gmail.com>
2023-05-17 05:35:54 +0200

    Configure Spring dependencies

---
+++ ecommerce-api/src/main/java/org/paradigmadigital/ecommerce/EcommerceWithHexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package org.paradigmadigital.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWithHexagonalArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWithHexagonalArchitectureApplication.class, args);
+	}
+
+}
